c255f58a32448e8e037c193846eb89ec4082ea3c
Anirudh J <anirudhjaishankar97@gmail.com>
2021-04-05 02:48:10 +0530

    update service and gallery look in all devices

--- index.js
+++ index.js
@@ -7,51 +7,52 @@ function toggleSidenav() {
     sidenav.classList.toggle('side-nav-close');
 }
 
-function getOffset(el) {
-    var _x = 0;
-    var _y = 0;
-    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
-}
-
 function moveSlide(dir) {
     var leftSlide = document.getElementById('slide-left');
     var centerSlide = document.getElementById('slide-center');
     var rightSlide = document.getElementById('slide-right');
 
-    if (dir === 'Right') {
+    if (dir === 'Left') {
         leftSlide.id = 'slide-center';
         centerSlide.id ='slide-right';
         rightSlide.id = 'slide-left';
-    } else if (dir === 'Left') {
+    } else if (dir === 'Right') {
         leftSlide.id = 'slide-right';
         centerSlide.id ='slide-left';
         rightSlide.id = 'slide-center';
     }
 }
 
+var currentActiveSlide = 2;
+
 function moveGallery(dir) {
-    var left2Image = document.getElementById('gallery-image-left-2');
-    var left1Image = document.getElementById('gallery-image-left-1');
-    var centerImage = document.getElementById('gallery-image-center');
-    var right1Image = document.getElementById('gallery-image-right-1');
-    var right2Image = document.getElementById('gallery-image-right-2');
+
+    var gallerySlides = document.getElementsByClassName('gallery-main-image');
+    var gallerySecondSlides = document.getElementsByClassName('gallery-image');
+
+    gallerySlides[currentActiveSlide].classList.toggle('hide');
+    gallerySecondSlides[currentActiveSlide].classList.toggle('image-selected');
+
 
     if (dir === 'Left') {
-        left2Image.id = 'gallery-image-left-1';
-        left1Image.id = 'gallery-image-center';
-        centerImage.id = 'gallery-image-right-1';
-        right1Image.id = 'gallery-image-right-2';
-        right2Image.id = 'gallery-image-left-2';
+        currentActiveSlide = currentActiveSlide == 0 ? gallerySlides.length - 1: currentActiveSlide - 1;
     } else if (dir === 'Right') {
-        left2Image.id = 'gallery-image-right-2';
-        left1Image.id = 'gallery-image-left-2';
-        centerImage.id = 'gallery-image-left-1';
-        right1Image.id = 'gallery-image-center';
-        right2Image.id = 'gallery-image-right-1';
+        currentActiveSlide = currentActiveSlide == gallerySlides.length-1 ? 0: currentActiveSlide + 1;
     }
+    gallerySlides[currentActiveSlide].classList.toggle('hide');
+    gallerySecondSlides[currentActiveSlide].classList.toggle('image-selected');
+
+}
+
+function galleryImageClick(imageNumber) {
+    var galleryMainSlides = document.getElementsByClassName('gallery-main-image');
+    var gallerySlides = document.getElementsByClassName('gallery-image');
+
+    galleryMainSlides[currentActiveSlide].classList.toggle('hide');
+    gallerySlides[currentActiveSlide].classList.toggle('image-selected');
+
+    currentActiveSlide = imageNumber-1;
+
+    galleryMainSlides[currentActiveSlide].classList.toggle('hide');
+    gallerySlides[currentActiveSlide].classList.toggle('image-selected');
 }
